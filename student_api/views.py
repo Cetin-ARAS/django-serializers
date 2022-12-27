becc1b1ffc977b2047ffd5b6779934224a650a84
ÇETİN <cetinnnarasss@gmail.com>
2022-12-27 14:52:59 +0300

    imports

--- student_api/views.py
+++ student_api/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
-
-from .models import Student, Path
-
-from .serializers import StudentSerializer, PathSerializer
+# rest framework import
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import render, HttpResponse, get_object_or_404
+#my imports
+from .models import Student, Path
+from .serializers import StudentSerializer, PathSerializer
 
+#!########################FUNCTİON BASED VİEWS####################################
 
 @api_view()  # default GET
 def home(requst):
@@ -20,58 +21,58 @@ def home(requst):
 # - delete (dB de kayıt silme)
 # - patch (kısmi update)
 
-# @api_view(['GET'])
-# def students_list(request):
-#     students = Student.objects.all()
-#     # print(students)
-#     serializer = StudentSerializer(students, many=True)
-#     # print(serializer)
-#     # print(serializer.data)
-#     return Response(serializer.data)
+@api_view(['GET'])
+def students_list(request):
+    students = Student.objects.all()
+    # print(students)
+    serializer = StudentSerializer(students, many=True)
+    # print(serializer)
+    # print(serializer.data)
+    return Response(serializer.data)
 
 
-# @api_view(['POST'])
-# def student_create(request):
-#     serializer = StudentSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         message = {
-#             "message": f'Student created succesfully....'
-#         }
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['POST'])
+def student_create(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message": f'Student created succesfully....'
+        }
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['GET'])
-# def student_detail(request, pk):
+@api_view(['GET'])
+def student_detail(request, pk):
 
-#     student = get_object_or_404(Student, id=pk)
-#     # student = Student.objects.get(id=pk)
-#     serializer = StudentSerializer(student)
-#     return Response(serializer.data)
-
-
-# @api_view(['PUT'])
-# def student_update(request, pk):
-#     student = get_object_or_404(Student, id=pk)
-#     serializer = StudentSerializer(instance=student, data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         message = {
-#             "message": f'Student updated succesfully....'
-#         }
-#         return Response(serializer.data)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    student = get_object_or_404(Student, id=pk)
+    # student = Student.objects.get(id=pk)
+    serializer = StudentSerializer(student)
+    return Response(serializer.data)
 
 
-# @api_view(['DELETE'])
-# def student_delete(request, pk):
-#     student = get_object_or_404(Student, id=pk)
-#     student.delete()
-#     message = {
-#         "message": 'Student deleted succesfully....'
-#     }
-#     return Response(message)
+@api_view(['PUT'])
+def student_update(request, pk):
+    student = get_object_or_404(Student, id=pk)
+    serializer = StudentSerializer(instance=student, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        message = {
+            "message": f'Student updated succesfully....'
+        }
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def student_delete(request, pk):
+    student = get_object_or_404(Student, id=pk)
+    student.delete()
+    message = {
+        "message": 'Student deleted succesfully....'
+    }
+    return Response(message)
 
 
 #############################################################
@@ -123,3 +124,5 @@ def student_api_get_update_delete(request, pk):
             "message": f"Student {student.last_name} deleted successfully"
         }
         return Response(data)
+#!########################CLASS BASED VİEWS####################################
+
