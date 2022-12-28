d7067187a9da90dd79c2865b77a350925adcc464
ÇETİN <cetinnnarasss@gmail.com>
2022-12-28 05:47:54 +0300

    views

--- student_api/views.py
+++ student_api/views.py
@@ -237,3 +237,42 @@ class StudentDetailCV(RetrieveUpdateDestroyAPIView):
     
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    
+
+#! ViewSets
+
+# - Django REST framework allows you to combine the logic for a set of related views in a single class, called a ViewSet. 
+
+# - Typically, rather than explicitly registering the views in a viewset in the urlconf, you'll register the viewset with a router class, that automatically determines the urlconf for you.
+
+# There are two main advantages of using a ViewSet class over using a View class.
+
+#  - Repeated logic can be combined into a single class. In the above example, we only need to specify the queryset once, and it'll be used across multiple views.
+#  - By using routers, we no longer need to deal with wiring up the URL conf ourselves.
+
+# Both of these come with a trade-off. Using regular views and URL confs is more explicit and gives you more control. ViewSets are helpful if you want to get up and running quickly, or when you have a large API and you want to enforce a consistent URL configuration throughout.
+
+
+class StudentMVS(ModelViewSet):
+    
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+    @action(detail=False, methods=["GET"])
+    def student_count(self, request):
+        count = {
+            "student-count" : self.queryset.count()
+        }
+        return Response(count)
+    
+    
+class PathMVS(ModelViewSet):
+
+    queryset = Path.objects.all()
+    serializer_class = PathSerializer
+    
+    @action(detail=True)
+    def student_names(self, request, pk=None):
+        path = self.get_object()
+        students = path.students.all()
+        return Response([i.first_name for i in students])
